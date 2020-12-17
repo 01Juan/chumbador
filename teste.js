a8e5c4727aac7a8395b83b06cfe60400731149c2
01Juan <65143979+01Juan@users.noreply.github.com>
2020-12-16 22:34:47 -0300

    Corrigido campos de entrada, luva e espço da nota

--- teste.js
+++ teste.js
@@ -7,11 +7,11 @@ function calcularRosca(){
         embutimento: parseFloat(document.getElementById('embutimento').value),
         chapa: parseFloat(document.getElementById('chapa').value),
         grout: parseFloat(document.getElementById('grout').value),
-        hPerfil: parseFloat(document.getElementById('hPerfil').value),
+        /* hPerfil: parseFloat(document.getElementById('hPerfil').value),
         teflon: parseFloat(document.getElementById('teflon').value),
         espessura: parseFloat(document.getElementById('espessura').value),
         comprimento: parseFloat(document.getElementById('comprimento').value),
-        largura: parseFloat(document.getElementById('largura').value),
+        largura: parseFloat(document.getElementById('largura').value), */
         quantidade: parseFloat(document.getElementById('quantidade').value),
 
         subtipoValor: parseFloat(subtipo.value),
@@ -49,15 +49,17 @@ function calcularRosca(){
             do {
                 diam = bitola[i][0];
                 if (diam === chumbador.diametroValor) {
-                    let a = bitola[i][21]
+                    let coluna = 21
+                    let a = bitola[i][coluna]
                     document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", "")})
                     document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", a)})
-                    let b = bitola[i][22]
+                    let b = bitola[i][coluna + 1]
                     document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", "")})
                     document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", b)})
-                    let ri = bitola[i][23]
+                    let ri = bitola[i][coluna + 2]
                     document.querySelector('.ri').setAttribute("value", "")
                     document.querySelector('.ri').setAttribute("value", ri)
+                    
                     let pesoArruela = bitola[i][18]
                     let pesoPorca = bitola[i][10]
                     document.querySelector('.diamSimbolo').setAttribute("value", "")
@@ -69,10 +71,11 @@ function calcularRosca(){
 
         rosca: function () {
             document.querySelectorAll('.rosca').forEach((e)=>{e.setAttribute("value", "")})
-            if (chumbador.projecao && chumbador.chapa){
+            if (chumbador.projecao && chumbador.chapa && chumbador.grout){
+                document.querySelectorAll('.rosca').forEach((e)=>{e.setAttribute("value", chumbador.projecao - chumbador.chapa - chumbador.grout)})
+            } else if (chumbador.projecao && chumbador.chapa){
                 document.querySelectorAll('.rosca').forEach((e)=>{e.setAttribute("value", chumbador.projecao - chumbador.chapa)})
             } else if (chumbador.projecao) {
-                let aaa = [123, 111]
                 document.querySelectorAll('.rosca').forEach((e)=>{e.setAttribute("value", chumbador.projecao)})
             }
         },
@@ -84,33 +87,35 @@ function calcularRosca(){
             do {
                 subt0 = subtipoLista[i][0]
                 subt1 = subtipoLista[i][1]
-        
+
+                //Poderia ser usado a linha abaixo e eliminar o DO acima também, porem se a tabela da luva for atualizada, nada mudaria aqui.
+                // if (chumbador.subtipoValor % 2 === 0) {
                 if (subt0 === chumbador.subtipoValor && subt1 === "Sim") {
-            /* if (chumbador.subtipoValor % 2 === 0) { */
                     let i = 0
                     let bitola = listaAtributos.slice(0,15)
                     do {
                         diam = bitola[i][0];
                         if (diam === chumbador.diametroValor) {
-                            let d = bitola[i][46]
+                            let coluna = 46
+                            let d = bitola[i][coluna]
                             document.querySelectorAll('.dadosSaidaLuva .d').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .d').forEach((e)=>{e.setAttribute("value", d)})
-                            let ee = bitola[i][47]
+                            let ee = bitola[i][coluna + 1]
                             document.querySelectorAll('.dadosSaidaLuva .e').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .e').forEach((e)=>{e.setAttribute("value", ee)})
-                            let f = bitola[i][48]
+                            let f = bitola[i][coluna + 2]
                             document.querySelectorAll('.dadosSaidaLuva .f').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .f').forEach((e)=>{e.setAttribute("value", f)})
-                            let g = bitola[i][49]
+                            let g = bitola[i][coluna + 3]
                             document.querySelectorAll('.dadosSaidaLuva .g').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .g').forEach((e)=>{e.setAttribute("value", g)})
-                            let h = bitola[i][50]
+                            let h = bitola[i][coluna + 4]
                             document.querySelectorAll('.dadosSaidaLuva .h').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .h').forEach((e)=>{e.setAttribute("value", h)})
-                            let ii = bitola[i][51]
+                            let ii = bitola[i][coluna + 5]
                             document.querySelectorAll('.dadosSaidaLuva .i').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .i').forEach((e)=>{e.setAttribute("value", ii)})
-                            let j = bitola[i][52]
+                            let j = bitola[i][coluna + 6]
                             document.querySelectorAll('.dadosSaidaLuva .j').forEach((e)=>{e.setAttribute("value", "")})
                             document.querySelectorAll('.dadosSaidaLuva .j').forEach((e)=>{e.setAttribute("value", j)})
 
