23ea8d50259ad0ed6ed91d74c1f9d98e833b7336
01Juan <65143979+01Juan@users.noreply.github.com>
2020-12-25 23:12:20 -0300

    Incluído transição de imagem

--- teste.js
+++ teste.js
@@ -13,40 +13,15 @@ function calcularRosca(){
         comprimento: parseFloat(document.getElementById('comprimento').value),
         largura: parseFloat(document.getElementById('largura').value), */
         quantidade: parseFloat(document.getElementById('quantidade').value),
-        
+
         tipoValor: tipo.options[tipo.selectedIndex].value,
         subtipoValor: parseFloat(subtipo.value),
         diametroValor: parseFloat(diametro.value),
         projecaoValor: parseFloat(projecao.value),
         embutimentoValor: parseFloat(embutimento.value),
 
-/*         abcdefghijklnRi: function () {
-            let i = 0
-            let bitola = listaAtributos.slice(0,15)
-            do {
-                diam = bitola[i][0];
-                if (diam === diametroValor) {
-                    let coluna = 21
-                    let a = bitola[i][coluna]
-                    document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", "")})
-                    document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", a)})
-                    let b = bitola[i][coluna + 1]
-                    document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", "")})
-                    document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", b)})
-                    let ri = bitola[i][coluna + 2]
-                    document.querySelector('.ri').setAttribute("value", "")
-                    document.querySelector('.ri').setAttribute("value", ri)
-                    
-                    let pesoArruela = bitola[i][18]
-                    let pesoPorca = bitola[i][10]
-                    document.querySelector('.diamSimbolo').setAttribute("value", "")
-                    document.querySelector('.diamSimbolo').setAttribute("value", bitola[i][0])
-                }
-                i++
-            } while (i < bitola.length)
-        }, */
-
-        defghij: function () {
+        //Preenche os atributos
+        atributos: function () {
             let i = 0
             let bitola = listaAtributos.slice(0,15)
             do {
@@ -65,6 +40,7 @@ function calcularRosca(){
                         document.querySelectorAll('.ri').forEach((e)=>{e.setAttribute("value", "")})
                     }
 
+                    //Indica a coluna que deve começar a pegar os dados para cada chumbador
                     if (chumbador.tipoValor === "I") {
                         coluna = 21
                     } else if (chumbador.tipoValor === "K") {
@@ -73,6 +49,7 @@ function calcularRosca(){
                         coluna = 36
                     }
 
+                    //Preenche os atributos (a b) do tipo I, K e M
                     let a = bitola[i][coluna]
                     document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", "")})
                     document.querySelectorAll('.a').forEach((e)=>{e.setAttribute("value", a)})
@@ -80,71 +57,65 @@ function calcularRosca(){
                     document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", "")})
                     document.querySelectorAll('.b').forEach((e)=>{e.setAttribute("value", b)})
 
+                    //Preenche os atributos (c d e) do tipo K e M
                     if (chumbador.tipoValor === "K" || chumbador.tipoValor === "M") {
                         let c = bitola[i][coluna + 2]
-                                                document.querySelectorAll('.c').forEach((e)=>{e.setAttribute("value", c)})
+                        document.querySelectorAll('.c').forEach((e)=>{e.setAttribute("value", c)})
                         let d = bitola[i][coluna + 3]
-                                                document.querySelectorAll('.d').forEach((e)=>{e.setAttribute("value", d)})
+                        document.querySelectorAll('.d').forEach((e)=>{e.setAttribute("value", d)})
                         let ee = bitola[i][coluna + 4]
-                                                document.querySelectorAll('.e').forEach((e)=>{e.setAttribute("value", ee)})
+                        document.querySelectorAll('.e').forEach((e)=>{e.setAttribute("value", ee)})
 
+                        //Preenche os atributos (f g h i j k l n) do tipo M
                         if (chumbador.tipoValor === "M") {
                             let f = bitola[i][coluna + 5]
-                            
                             document.querySelectorAll('.f').forEach((e)=>{e.setAttribute("value", f)})
                             let g = bitola[i][coluna + 6]
-                            
                             document.querySelectorAll('.g').forEach((e)=>{e.setAttribute("value", g)})
                             let h = bitola[i][coluna + 7]
-                            
                             document.querySelectorAll('.h').forEach((e)=>{e.setAttribute("value", h)})
                             let ii = bitola[i][coluna + 8]
-                            
                             document.querySelectorAll('.i').forEach((e)=>{e.setAttribute("value", ii)})
                             let j = bitola[i][coluna + 9]
-                            
                             document.querySelectorAll('.j').forEach((e)=>{e.setAttribute("value", j)})
                             let k = bitola[i][coluna + 10]
-                            
                             document.querySelectorAll('.k').forEach((e)=>{e.setAttribute("value", k)})
-                            let l = bitola[i][coluna + 11]
-                            
-                            document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", l)})
-                            let n = bitola[i][coluna + 12]
-                            
+                            if (chumbador.embutimento) {
+                                document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", chumbador.embutimento)})
+                            }
+                            let n = bitola[i][coluna + 11]
                             document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", n)})
                         } else {
-                            
-document.querySelectorAll('.f').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.g').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.h').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.i').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.j').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.k').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", "")})
+                            //Apaga os atributos (f g h i j k l n)
+                            document.querySelectorAll('.f').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.g').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.h').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.i').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.j').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.k').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", "")})
+                            document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", "")})
                         }
                     } else {
+                        //Apaga os atributos (c d e f g h i j k l n)
                         document.querySelectorAll('.c').forEach((e)=>{e.setAttribute("value", "")})
-
-document.querySelectorAll('.d').forEach((e)=>{e.setAttribute("value", "")})
-
-document.querySelectorAll('.e').forEach((e)=>{e.setAttribute("value", "")})
-
-document.querySelectorAll('.f').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.g').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.h').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.i').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.j').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.k').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", "")})
-document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.d').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.e').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.f').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.g').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.h').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.i').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.j').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.k').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.l').forEach((e)=>{e.setAttribute("value", "")})
+                        document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", "")})
                     }
                 }
                 i++
             } while (i < bitola.length)
         },
 
+        //Calcula o comprimento da rosca
         rosca: function () {
             document.querySelectorAll('.rosca').forEach((e)=>{e.setAttribute("value", "")})
             if (chumbador.projecao && chumbador.chapa && chumbador.grout){
@@ -208,10 +179,90 @@ document.querySelectorAll('.n').forEach((e)=>{e.setAttribute("value", "")})
                 }
                 i++
             } while (i < subtipoLista.length)
+        },
+
+        //Preenche a tabela com os dados
+        tabela: function () {
+            let ii = 1
+            for (let i = 0; i <= 50; i = 7 + i) {
+                //Linha
+                document.getElementsByClassName("resultado")[i].innerHTML = ii
+                ii++
+
+                //QTD
+                if (i + 1 === 8) {
+                    let subtipoLista = subtipos.slice(0,8)
+                    for (let iii = 0; iii < 8;iii++) {
+                        subt0 = subtipoLista[iii][0]
+                        subt4 = subtipoLista[iii][4]
+                        if (subt0 === chumbador.subtipoValor) {
+                            document.getElementsByClassName("resultado")[i + 1].innerHTML = chumbador.quantidade * subt4
+                        }
+                    }
+                } else if (i + 1 === 29) {
+                    document.getElementsByClassName("resultado")[i + 1].innerHTML = chumbador.quantidade * 4
+                } else {
+                    document.getElementsByClassName("resultado")[i + 1].innerHTML = chumbador.quantidade
+                }
+
+                //Descrição
+                if (i + 2 === 2) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "CHUMBADOR Ø " + chumbador.diametro + "mm"
+                } else if (i + 2 === 9) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "PORCA SEXTAVADA PESADA"
+                } else if (i + 2 === 16) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "ARRUELA CIRCULAR LISA"
+                } else if (i + 2 === 23) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "CHAPA # " + parseFloat(document.querySelectorAll('.dadosSaidaChumbador .b')[1].value) + "mm"
+                } else if (i + 2 === 30) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "CHAPA # " + parseFloat(document.querySelectorAll('.dadosSaidaChumbador .h')[1].value) + "mm"
+                } else if (i + 2 === 37) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "CHAPA # " + parseFloat(document.querySelectorAll('.dadosSaidaChumbador .k')[1].value) + "mm"
+                } else if (i + 2 === 44) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "TUBO  Ø " + parseFloat(document.querySelectorAll('.dadosSaidaLuva .f')[1].value) + "mm"
+                } else if (i + 2 === 51) {
+                    document.getElementsByClassName("resultado")[i + 2].innerHTML = "CHAPA # " + chumbador.chapa + "mm"
+                }
+
+                //Comp. / Área
+                if ((chumbador.tipoValor === "K" || chumbador.tipoValor === "M") && i + 3 === 3) {
+                    document.getElementsByClassName("resultado")[i + 3].innerHTML = chumbador.projecao + chumbador.embutimento
+                } else {
+                    document.getElementsByClassName("resultado")[i + 3].innerHTML = "Pendente"
+                }
+                //Massa Unit.
+                document.getElementsByClassName("resultado")[i + 4].innerHTML = 555
+                //Massa Total
+                document.getElementsByClassName("resultado")[i + 5].innerHTML = 666
+
+                //Especificação
+                if (i + 6 === 6) {
+                    document.getElementsByClassName("resultado")[i + 6].innerHTML = "ASTM A36"
+                } else if (i + 6 === 13) {
+                    document.getElementsByClassName("resultado")[i + 6].innerHTML = "ASTM A 194 Gr.2H"
+                } else if (i + 6 === 20) {
+                    document.getElementsByClassName("resultado")[i + 6].innerHTML = "ASTM A36"
+                } else if (i + 6 === 27) {
+                    document.getElementsByClassName("resultado")[i + 6].innerHTML = "ASTM A36"
+                } else {
+                    document.getElementsByClassName("resultado")[i + 6].innerHTML = ""
+                }
+            }
         }
     }
 
-    chumbador.defghij()
+    chumbador.atributos()
     chumbador.rosca()
     chumbador.luva()
+    chumbador.tabela()
 }
+
+window.onload = function(){ 
+    var clique = document.getElementById("btn-menu");
+    var menuLateral = document.getElementById("menu-lateral");
+    
+    clique.onclick = function (e) {
+        e.preventDefault();
+        menuLateral.classList.toggle('toggleMenu');
+    };
+};
